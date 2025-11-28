72d55d20d16667c9d8df3617f2949ecd272103b0
xadeayox <162280997+xadeayox@users.noreply.github.com>
2025-11-28 22:42:35 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+pip install xgboost
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -135,3 +136,4 @@ st.pyplot()
 
 st.write("---")
 st.write("Built with Streamlit + XGBoost + SHAP")
+
